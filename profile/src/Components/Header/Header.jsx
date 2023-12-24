92c3a0ab0e73d87547b1031762fdc414381f0c49
GiftSibiya <sibiya.bobo@gmail.com>
2023-12-24 13:57:25 +0200

    homepage setup

---
+++ profile/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+// Depandancies Imports //
+import React from "react";
+//*//
+
+// File Imports //
+import "./Header.css";
+
+//*//
+
+function Header() {
+  return <section className="headerSection">Header Here</section>;
+}
+
+export default Header;
---
+++ profile/src/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,24 @@
+// Dependancies Imports //
+import React from "react";
+
+//**//
+
+// Components Imports //
+import Header from "../Components/Header/Header";
+
+//**//
+
+//Main Code//
+function HomeScreen() {
+  return (
+    <>
+      <Header>
+        <Header />
+      </Header>
+    </>
+  );
+}
+
+//**//
+
+export default HomeScreen;
