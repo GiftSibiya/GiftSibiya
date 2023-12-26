95d2d481f883e72e8b5396f0d5febcda0d9d5dda
GiftSibiya <sibiya.bobo@gmail.com>
2023-12-26 14:17:00 +0200

    finished profile component

--- profile/src/Components/Projects/ProjectCard/ProjectCard.jsx
+++ profile/src/Components/Projects/ProjectCard/ProjectCard.jsx
@@ -10,6 +10,7 @@ import JavaScript from "../../logoComponent/JavaScript";
 import ReactIcon from "../../logoComponent/ReactIcon";
 import NodeIcon from "../../logoComponent/NodeIcon";
 import CsIcon from "../../logoComponent/CsIcon";
+import github from "../../../assets/icons/github.png";
 import Darticon from "../../logoComponent/Darticon";
 import AndroidStudio from "../../logoComponent/AndroiStudio";
 //--//
@@ -18,19 +19,23 @@ function ProjectCard(props) {
   return (
     <div className="cardContainer">
       <div className="projectCard">
-        <div className="projectCard__pic">pic here</div>
+        <div className="projectCard__pic">{props.screenshot}</div>
         <div className="projectCard__info">
           <div className="projectCard__info__title"> {props.title}</div>
           <div className="projectCard__info__description">
             {props.description}
           </div>
-          <div className="projectCard__info__stack">
-            <Darticon />
-            <AndroidStudio />
-          </div>
+          <div className="projectCard__info__stack"> {props.techStack}</div>
           <div className="projectCard__info__links">
-            <div className="projectLink1"></div>
-            <div className="projectLink2">Demo Here</div>
+            <div className="projectLinks">
+              <a className="linkButton gitLink" href="">
+                <img className="buttonIcon" src={github} alt="" />
+                <div className="buttonItems">Code Here</div>
+              </a>
+              <a className="linkButton" href="">
+                Demo Here
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- profile/src/Components/Projects/Projects.jsx
+++ profile/src/Components/Projects/Projects.jsx
@@ -9,17 +9,34 @@ import React from "react";
 import "./Projects.css";
 import ProjectCard from "./ProjectCard/ProjectCard";
 
+// assets //
+import frontSeat1 from "../../assets/images/light_screenshot.png";
+import Darticon from "../logoComponent/Darticon";
+import AndroidStudio from "../logoComponent/AndroiStudio";
+
 //--//
 
 function Projects() {
   return (
     <section className="projects">
       <h2 className="projects__title">Projects</h2>
-      <ProjectCard
-        title="FrontSeat"
-        description="Mobile Taxi Maths calculator"
-        code="52"
-      />
+      <h5 className="projects__description">Projects</h5>
+      <div className="projectsCard">
+        <ProjectCard
+          screenshot=""
+          title="FrontSeat"
+          description="The mobile version of my web Taxi Maths 
+          calculator taken from html and css to Dart and flutter"
+          techStack={[<Darticon key="dart" />, <AndroidStudio key="android" />]}
+        />
+        <ProjectCard
+          screenshot=""
+          title="FrontSeat"
+          description="The mobile version of my web Taxi Maths 
+          calculator taken from html and css to Dart and flutter"
+          techStack={[<Darticon key="dart" />, <AndroidStudio key="android" />]}
+        />
+      </div>
     </section>
   );
 }
